66240806ea6fbab698fd5d81509f17d317fb6587
Pedro Luis <pedrolcr@outlook.com>
2020-12-06 01:26:41 -0300

    Start Project

--- README.md
+++
@@ -1,2 +0,0 @@
-# Dreams-Bot
-The best discord bot
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv';
+import Discord from 'discord.js';
+
+dotenv.config();
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	if (client) {
+		console.log(`Logged in as ${client.user.tag}!`);
+	} else {
+		console.log(`Cliente Discord Falhou!`);
+	}
+});
+
+client.on('message', msg => {
+	if (msg.content === 'ping') {
+		msg.reply('Pong!');
+	}
+});
+
+client.login(process.env.DISC_TOKEN);
