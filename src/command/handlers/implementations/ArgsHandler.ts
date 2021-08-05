959f4d027a47226aed21da39cf1ab59ab2d58a8a
Pedro Luis <pedrolcr@outlook.com>
2021-08-04 22:55:39 -0300

    Small visual fixes

--- src/command/handlers/implementations/ArgsHandler.ts
+++ src/command/handlers/implementations/ArgsHandler.ts
@@ -5,30 +5,14 @@ import { AbstractHandler } from "../AbstractHandler";
 class ArgsHandler extends AbstractHandler{
 
   async handle(msg: Discord.Message) : Promise<void>{
+
+
     // filtter args handler
-    if (
-      msg.command.args !== undefined &&
-      msg.command.args === true &&
-      msg.args.length === 0
-    ) {
-      const reply = `You didn't provide any arguments, ${msg.author}!\n
-        The proper usage would be: \`${msg.guild.prefix}${msg.command.name}
-        ${msg.command.usage ? msg.command.usage : ''}\``;
-
+    if ((msg.command.args && msg.args.length === 0) || !msg.command.args && msg.args.length !== 0) {
+      const reply = `You didn't provide any arguments, ${msg.author}!\n` +
+        `The proper usage would be: `+
+        `\`${msg.guild.prefix}${msg.command.name} ${msg.command.usage ? msg.command.usage : ''}\``;
       throw new CommandError(reply ,msg.channel );
-    }
-
-    if (
-      msg.command.args !== undefined &&
-      msg.command.args === false &&
-      msg.args.length !== 0
-    ) {
-      const reply = `This command does not require any arguments,
-        ${msg.author}!\n
-        The proper usage would be: \`${msg.guild.prefix}${msg.command.name}
-        ${msg.command.usage ? msg.command.usage : ''}\``;
-
-      throw new CommandError(reply, msg.channel );
     }
 
     await super.handle(msg);
